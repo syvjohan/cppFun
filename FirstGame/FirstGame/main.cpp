c79118a2bac693b45acd2c6dd305159a8d03a296
syvjohan <johanfredriksson1987@gmail.com>
2014-04-09 10:48:07 +0200

    start implement barriers

--- FirstGame/FirstGame/main.cpp
+++ FirstGame/FirstGame/main.cpp
@@ -23,8 +23,7 @@ struct rectangle {
 	point height;
 } player, barrier;
 
-const int playerSize = 40;
-const int barrierSize = 40;
+const int rectSize = 40;
 rectangle arr[];
 
 void fillRect(int x, int y, int w, int h, int color) {
@@ -32,26 +31,38 @@ void fillRect(int x, int y, int w, int h, int color) {
 	SDL_FillRect(surface, &rect, color);
 }
 
-void createBarrier() {
+rectangle barrierRect() {
+	int pX;
+	int pY;
 	for (int i = 0; i != 10; i++) {
-		arr[i];
+		pX = (barrier.width.x = (rand()  % 1180 + 50));
+		pY = (barrier.height.y = (rand() % 620 + 50));
+		arr[px];
+		arr[pY];
+		fillRect(pX, pY, rectSize, rectSize, RGB_COLOR(255, 0, 255));
 	}
-	// TODO slumpa ut pos på skärmen x och y kordinater fyll i fillrect
-	fillRect(barrier.width.x, barrier.height.y, barrierSize, barrierSize, RGB_COLOR(255, 255, 0));
+
+	for (
+
+	return barrier;
+}
+
+void playerRect() {
+		fillRect(player.width.x, player.height.y, rectSize, rectSize, RGB_COLOR(255, 255, 0));
 }
 
 void wallCollision() {
 	if (player.width.x > screenWidth) {
-			player.width.x= screenWidth - playerSize;
+			player.width.x= screenWidth - rectSize;
 		}
 		if (player.height.y > screenHeight) {
-			player.height.y = screenHeight - playerSize;
+			player.height.y = screenHeight - rectSize;
 		}
 		if (player.width.x < 0) {
-			player.width.x = 0 + playerSize;
+			player.width.x = 0 + rectSize;
 		}
 		if (player.height.y < 0) {
-			player.height.y = 0 + playerSize;
+			player.height.y = 0 + rectSize;
 		}
 		return;
 }
@@ -93,13 +104,12 @@ int main() {
 			}	
 		}
 
-		controlls();
+		fillRect(50, 50, screenWidth, screenHeight, 0);
+		barrierRect();
+		playerRect();
 
+		controlls();
 		wallCollision();
-	
-		fillRect(10, 10, screenWidth, screenHeight, 0);
-		fillRect(player.width.x, player.height.y, playerSize, playerSize, RGB_COLOR(255, 255, 0));
-		createBarrier();
 
 		SDL_UpdateWindowSurface(window);
 		SDL_Delay(1);
