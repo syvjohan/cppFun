0e1a59ae9b7e8abe7858aa929b476f134d61f2a1
syvjohan <johanfredriksson1987@gmail.com>
2014-09-08 22:14:38 +0200

    !gg

--- Inlämningar/MyStringClass/MyStringClass/XString.cpp
+++ Inlämningar/MyStringClass/MyStringClass/XString.cpp
@@ -2,11 +2,12 @@
 #include "Defs.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
 
 String::String() {
 	nextIndex = 0;
-	sizes = 10;
-	str = DBG_NEW char[sizes]; //Create an object.
+	size = 11;
+	str = DBG_NEW char[size]; //Create an object.
 }
 
 String::~String() {
@@ -45,8 +46,13 @@ size_t String::StringSize(const char &arr) const {
 }
 
 //Erase all the elements in the char[].
-void String::StringClear(const int &arr) {
-
+char *String::StringClear(char *arr) {
+	if (StringIsEmpty(*arr) == false) {
+		for (int i = 0; arr[i] != '\0'; i++) {
+			arr[i] = '\0';
+		}
+		return arr;
+	}
 }
 
 //Check if char[] is empty.
@@ -58,18 +64,81 @@ bool String::StringIsEmpty(const char &arr) const {
 }
 
 //Shrink the char[] size
-void String::StringShrink(const int &shrink) {
-
+void String::StringShrink(char *arr) {
+	printf("\nWrong class use the Vector class instead!\n");
 }
 
-//Returns a specific position in char[].
-void String::StringPosition(const int &position) {
+// append element in the end of char[].
+char *String::StringAppend(char *arr, const char &addChar) {
+	if (StringIsEmpty(addChar) == false) {
 
+		int sumCount = 0;
+		//count elements to be added!
+		for (int i = 0; (&addChar)[i] != '\0'; i++) {
+			sumCount++;
+		}
+	
+		int newSize = (size + sumCount) * 2;
+		char *temp = DBG_NEW char[newSize];
+
+		//copy char[] into temp char[]
+		while ((arr)[nextIndex] != '\0') {
+			temp[nextIndex] = arr[nextIndex];
+			nextIndex++;
+		}
+
+		//copy the chars to be added and place them after the exisiting chars.
+		for (int i = 0; (&addChar)[i] != '\0'; i++, nextIndex++) {
+			temp[nextIndex] = (&addChar)[i];
+		}
+
+		//Fill the rest of the slots with '\0'.
+		for (nextIndex + 1; nextIndex != newSize; nextIndex++) {
+			temp[nextIndex] = '\0';
+		}
+
+		arr = temp;
+		size = newSize;
+		return arr;
+	}
 }
 
-// append element to char[].
-void String::StringPush(const char &push) {
+//Append element to char[] at a specific position and number of elements.
+char *String::StringInsertElement(char *arrInsertInto, const char &arrCopyFrom, int pos, int number) {
+/*	if (StringIsEmpty(arrCopyFrom) == false) {
 
+		int sumCount = 0;
+		//count elements to be added!
+		for (int i = 0; (&addChar)[i] != '\0'; i++) {
+			sumCount++;
+		}
+
+		int newSize = (size + sumCount) * 2;
+		char *temp = DBG_NEW char[newSize];
+
+		//copy char[] into temp char[]
+		while ((arr)[nextIndex] != '\0') {
+			temp[nextIndex] = arr[nextIndex];
+			nextIndex++;
+		}
+
+	
+		//copy the number of chars to be added and place them at position x.
+		for (int i = 0; i != '\0' || pos != '\0' || pos != number; i++, pos++) {
+
+			
+		}
+
+		//Fill the rest of the slots with '\0'.
+		for (nextIndex + 1; nextIndex != newSize; nextIndex++) {
+			temp[nextIndex] = '\0';
+		}
+
+		arr = temp;
+		size = newSize;
+		return arr;
+	}*/
+	return 0;
 }
 
 //Erase last element in char[] (string).
@@ -82,17 +151,26 @@ char *String::StringPop(char *arr) {
 			count++;
 		}
 
-		lastChar = arr[count - 1];
+		arr[count - 1] = lastChar;
 		return arr;
 	}
 }
 
 //Compare the content of 2 different char[]
-bool String::StringCompare(const char &str1, const char &str2) {
-	if (str1 == str2) {
-		return true;
+bool String::StringCompare(const char *str1, const char *str2) {
+	size_t len1 = strlen(str1);
+	size_t len2 = strlen(str2);
+
+	if (len1 != len2)
+		return false;
+
+	for (int i = 0; i != len1; ++i) {
+		if (*str1 != *str2) 
+			return false;
 	}
-	return false;
+	
+
+	return true;
 }
 
 //Copy the char[] into another char[]. Set start (pos) and number of chars to be copied.
--- Inlämningar/MyStringClass/MyStringClass/main.cpp
+++ Inlämningar/MyStringClass/MyStringClass/main.cpp
@@ -1,19 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
 #include "XString.h"
 #include "Defs.h"
+#include "PeterString.h"
+
+char *StrInsert(char *target, char *source, int index);
+
+void PeterTest() {
+	PString str = "Hej och ar fet";
+
+	//str.Append("Peter").Append(" och ditt ar ").Append("Johan");
+
+	PString p1 = str.SubString(0, 4);
+	p1.Append("peter");
+	PString p2 = str.SubString(3, str.Length() - 4);
+	p1.Append(p2);
+
+
+
+	str.Insert("peter ", 4);
+
+	//PString sub = str.SubString(4, 4);
+
+
+
+	printf("%s\n", str.CString());
+}
 
 int main() {
+	PeterTest();
 
-	const int size = 11;
-	char arr[size] = {'h','e', 'j', ' ','p', 'a', ' ', 'd', 'i', 'g', '\0'};
-	char arr2[size] = {};
+	//const int size = 33;
+	//char arr[size] = {'h','e', 'j', ' ','p', 'a', ' ', 'd', 'i', 'g', '\0'};
+	//char arr2[size] = {'j', 'o', 'h', 'a', 'n', '\0'};
+	//char arr3[size] = {};
 
+	//char *target = "Hej och Johan";
+	//char *source = "Peter ";
 
-	String string;
-	//printf("%c\n", string.StringLastChar(arr));
-	//printf("%i\n", string.StringSize(*arr));
-	//printf("%c", string.StringFirstChar(arr));
+	//StrInsert(target, source, 4);
+	//String string;
+
+	////printf("%c\n", string.StringLastChar(arr));
+	////printf("%i\n", string.StringSize(*arr));
+	////printf("%c", string.StringFirstChar(arr));
+
+	//string.StringAppend("hej", 'g');
 
 	/*if (string.StringIsEmpty(*arr) == false) {
 		printf("string is empty");
@@ -26,21 +59,38 @@ int main() {
 	//string.StringCopy(arr, *arr2, 2, 8);
 	//printf("%c", arr2);
 
-	string.StringPop(arr);
-	printf("%c", arr);
+	//printf(string.StringPop(arr));
+
+	//printf(string.StringAppend(arr, *arr2));
+	//printf(string.StringInsertElement(arr, *arr2, 0, 6));
+	
+	//printf(string.StringClear(arr));
+
+	//string.StringShrink(arr);
+
 
 	system("pause");
 	return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
 //_CrtDumpMemoryLeaks();
+
+
+char *StrInsert(char *target, char *source, int index) {
+
+	size_t sourceLen = strlen(source);
+	size_t targetLen = strlen(target);
+
+	//target  H | E | J | | J | O | H | A | N |
+	// Index =              4   
+ 	//source | P | E | T | E | R
+
+	char *buffer = new char[sourceLen + targetLen];
+
+	memcpy(buffer, target, index);
+	memcpy(buffer + index, source, sourceLen);
+	memcpy(buffer + index + sourceLen, target + index, targetLen - index + 1);
+
+
+	return buffer;
+}
