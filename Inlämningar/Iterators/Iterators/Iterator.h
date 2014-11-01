14adcf34954925b72bd58b58c20a6cfa73ac70f5
syvjohan <johanfredriksson1987@gmail.com>
2014-11-01 15:40:26 +0100

    implemented all functions but nothing has been tested so there might be problem with functionalitet and design

--- Inlämningar/Iterators/Iterators/Iterator.h
+++ Inlämningar/Iterators/Iterators/Iterator.h
@@ -9,32 +9,54 @@ bool operator==(const T &lhs, const T &rhs);
 template<typename T>
 bool operator!=(const T &lhs, const T &rhs);
 
-using namespace std;
-
 template<typename T>
 class Iterator {
 public:
-	Iterator(char *str);
-	~Iterator();
+	Iterator<T>(T *obj);
+	~Iterator<T>();
+
+	typedef Iterator<T> ForwardIterator;
+	typedef Iterator<T> ReverseIterator;
+	typedef Iterator<const T> ConstIterator;
+	typedef Iterator<const T> ConstReverseIterator;
 
 	//Operators
 	friend bool operator==(const T &lhs, const T &rhs);
 	friend bool operator!=(const T &lhs, const T &rhs);
 
-	Iterator<T> operator+(const Iterator<T> &it);
 	Iterator<T>& operator++(); //Prefix
 	Iterator<T> operator++(int); //Postfix
+
+	Iterator<T>& operator--(); //Prefix
+	Iterator<T> operator--(int); //Postfix
+
+	Iterator<T> operator+(const Iterator<T> &it);
 	T& operator[](int i);
 	T& operator*();
 	const T& operator*() const;
 	//End operators
 
-	T* Begin();
-	T* End();
-	T* Prev();
-	T* Next();
+	//Forward iterators
+	T* ForwardBegin();
+	T* ForwardEnd();
+
+	//Reverse iterator
+	T* ReverseBegin();
+	T* ReverseEnd();
+
+	//Const iterator
+	T* ConstBegin();
+	T* ConstEnd();
+
+	//Const reverse iterator
+	T* ConstReverseBegin();
+	T* ConstReverseEnd();
 
 private:
+	T Size(const T *container) const;
+	int LastElement(const T *container);
+	bool CompareValues(const T *obj1, const T *obj2);
+
 	struct Container {
 		T *container;
 	}*con;
@@ -42,26 +64,17 @@ private:
 	struct IteratorData {
 		Container *first;
 		Container *last;
-		Container *next;
-		Container *prev;
+		Container *current;
 	}*iterData;
 };
 
-//iterator,
-//const_iterator,
-//reverse_iteraor,
-//const_reverse_iterator.
-
-
 template<typename T>
-Iterator<T>::Iterator(char *str) {
-	con->container = str;
+Iterator<T>::Iterator(T *obj) {
+	con->container = obj;
 
-	con->container->next = nullptr;
-	con->container->prev = nullptr;
-	con->container->last = nullptr;
-	con->container->first = nullptr;
-	
+	iterData->last = nullptr;
+	iterdat->first = nullptr;
+	iterData->current = nullptr;
 }
 
 template<typename T>
@@ -71,63 +84,177 @@ Iterator<T>::~Iterator() {
 
 template<typename T>
 Iterator<T> Iterator<T>::operator+(const Iterator<T> &it) {
-
+	return iterData->current + it;
 }
 
-//Prefix 
+//Prefix ++i.
 template<typename T>
 Iterator<T>& Iterator<T>::operator++() {
-
+	//Increment object.
+	if (iterData->current == iterData->last) {
+		return *this;
+	}
+	else {
+		++iterData->current;
+		return *this;
+	}
 }
 
-//Postfix
+//Postfix i++
 template<typename T>
 Iterator<T> Iterator<T>::operator++(int) {
+	if (iterData->current != iterData->last) {
+		T *tmp = IterData->current;
+		++iterdata->current;
+	}
+	else {
+		return *this;
+	}
+	
+	return tmp;
+}
 
+//Prefix --i.
+template<typename T>
+Iterator<T>& Iterator<T>::operator--() {
+	//Increment object.
+	if (iterData->current == iterData->first) {
+		return *this;
+	}
+	else {
+		--iterData->current;
+		return *this;
+	}
+}
+
+//Postfix i--.
+template<typename T>
+Iterator<T> Iterator<T>::operator--(int) {
+	if (iterData->current != iterData->first) {
+		T *tmp = IterData->current;
+		--iterdata->current;
+	}
+	else {
+		return *this;
+	}
+
+	return tmp;
 }
 
 template<typename T>
 T& Iterator<T>::operator[](int i) {
-	
+	return con->container[i];
 }
 
+//Return a reference to the value current points to.
 template<typename T>
 T& Iterator<T>::operator*() {
-
+	return *iterData->current;
 }
 
 template<typename T>
 const T& Iterator<T>::operator*() const {
+	return *iterData->current;
+}
 
+//Forward iterators
+template<typename T>
+T* Iterator<T>::ForwardBegin() {
+	iterData->first = con->container[0];
+	&iterData->current = iterData->first;
+
+	return ForwardIterator(&iterData->first);
 }
 
 template<typename T>
-T* Iterator<T>::Begin() {
-	con->container->first = con->container[0];
-	return iterData->first;
+T* Iterator<T>::ForwardEnd() {
+	iterData->last = Size(con->container);
+	&iterData->current = iterData->last;
+
+	return ForwardIterator(&iterData->last);
+}
+//END, Forward iterators
+
+//Reverse iterator
+template<typename T>
+T* Iterator<T>::ReverseBegin() {
+	iterData->last = Size(con->container);
+	&iterData->current = iterData->last;
+
+	return ReverseIterator(&iterData->last);
 }
 
 template<typename T>
-T* Iterator<T>::End() {
-	int len = strlen(con->container);
-	con->container->last = con->container[len];
+T* Iterator<T>::ReverseEnd() {
+	iterData->first = con->container[0];
+	&iterData->current = iterData->first;
 
-	return iterData->last;
+	return ReverseIterator(&iterData->first);
+}
+//END, Reverse iterator
+
+//Const iterator
+template<typename T>
+T* Iterator<T>::ConstBegin() {
+	return ConstIterator(ForwardBegin());
 }
 
 template<typename T>
-T* Iterator<T>::Next() {
-	return ++con->container->next;
+T* Iterator<T>::ConstEnd() {
+	return ConstIterator(ForwardEnd());
+}
+//END, Const iterator
+
+//Const reverse iterator
+template<typename T>
+T* Iterator<T>::ConstReverseBegin() {
+	return ConstReverseIterator(ReverseBegin());
 }
 
 template<typename T>
-T* Iterator<T>::Prev() {
-	return --con->container->prev;
+T* Iterator<T>::ConstReverseEnd() {
+	return ConstReverseIterator(ReverseEnd());
+}
+//END, Const reverse iterator
+
+template<typename T>
+inline int Iterator<T>::LastElement(const T *container) {
+	int last = (sizeof(container) / sizeof(container[0]) - 1);
+	return last;
+}
+
+template<typename T>
+inline T Iterator<T>::Size(const T *container) const {
+	int last = LastElement(container);
+	size_t length = 0;
+	while (*container != last) {
+		container++;
+		length++;
+	}
+
+	return length;
+}
+
+template<typename T>
+inline bool Iterator<T>::CompareValues(const T *obj1, const T *obj2) {
+	int last1 = LastElement(obj1);
+	int last2 = LastElement(obj2);
+	do {
+		if (*obj1 != *obj2) {
+			return false;
+		}
+	} while (++obj1 != last1 && ++obj2 != last2);
+
+	return true;
 }
 
 //Global scope!
 template<typename T>
-inline bool operator==(const T &lhs, const T &rhs);
+inline bool operator==(const T &lhs, const T &rhs) {
+	return Iterator<T>::CompareValues(lhs, rhs);
+}
 
 template<typename T>
-inline bool operator!=(const T &lhs, const T &rhs);
+inline bool operator!=(const T &lhs, const T &rhs) {
+	return !(lhs == rhs);
+}
--- Inlämningar/Iterators/Iterators/MainStringItt.cpp
+++ Inlämningar/Iterators/Iterators/MainStringItt.cpp
@@ -18,103 +18,103 @@ using namespace std;
 
 
 void TestFörGodkäntString() {
-//-	String()
-	string s0;	assert (s0=="");
-
-//-	String(Sträng sträng)
-	string s1("foo"); assert(s1=="foo");
-	string s2(s1); assert(s2=="foo");
-	string s3("bar");  assert(s3=="bar");
-
-//-	~String() Kom ihåg destruktorn!
-	delete new string("hej");
-
-//	-	operator =(Sträng sträng)
-	assert((s2=s3)==s3);
-	assert((s2=s2)==s3);
-	assert((s2=("foo"))=="foo");
-	assert((s2="bar")=="bar");
-
-//-	operator+=(Sträng sträng) som tolkas som konkatenering.
-	//foo, bar, bar
-	(s2+=s1)+=(s3+=s1);
-	assert(s3=="barfoo" && s2=="barfoobarfoo" && s1=="foo");
-
-	//+= som får plats;
-	s3="bar"; s3.reserve(10);
-	s3+=s1;
-	assert(s3=="barfoo");
-
-	//+= som inte får plats;
-	s3="bar"; s3.reserve(5);
-	s3+=s1;
-	assert(s3=="barfoo");
-
-	//+= som får plats; Själv
-	s3="bar"; s3.reserve(10);
-	s3+=s3;
-	assert(s3=="barbar");
-
-	//+= som inte får plats; Själv
-	s3="bar"; s3.reserve(5);
-	s3+=s3;
-	assert(s3=="barbar");
-
-//-	operator+ räcker med bara String+String
-	s2="bar";
-	//auto sss=s1+s2;
-	//sss=="foobar";
-	//assert(sss=="foobar");
-	////assert(s1+s2=="foobar" && s1=="foo");
-	assert(s1+s2=="foobar" && s1=="foo");
-
-//-	operator== räcker med String==Sträng
-	//testas överallt!
-
-//-	at(int i) som indexerar med range check
-	try {
-	 s2.at(-1);
-	 assert(false);
-	} catch (std::out_of_range&) {};
-	try {
-	 s2.at(3);
-	 assert(false);
-	} catch (std::out_of_range&) {};
-	assert(s2.at(2)='r');
-
-//-	operator[](int i) som indexerar utan range check.
-	s2[-1]; s2[1000];
-	assert(s2[1]=='a');
-
-//-	push_back(char c) lägger till ett tecken sist.
-	s2.push_back('a');
-	assert(s2=="bara");
-
-	//-	length(), reserve(), capacity() och shrink_to_fit() är funktioner som finns i container klasserna i STL.
-
-	int len=s2.length();
-	s2.shrink_to_fit();
-	assert(s2.length()==s2.capacity());
-
-	s2.data();
-	if(s2.length()==s2.capacity()) {
-		//lagrar strängen med \0
-		const char * p1 = s2.data();
-		s2.reserve(len); assert(p1==s2.data()); //no change
-		p1= s2.data(); s2.reserve(len+1); assert(p1!=s2.data()); //change
-		p1= s2.data(); s2.shrink_to_fit();  assert(p1!=s2.data()); //change
-		p1= s2.data(); s2.shrink_to_fit();  assert(p1==s2.data()); //no change
-	} else {
-		//lagrar strängen utan \0
-		int cap;
-		s2.data(); cap=s2.capacity(); s2.shrink_to_fit(); assert(cap!=s2.capacity()); //change
-		cap=s2.capacity(); s2.data(); assert(cap!=s2.capacity()); //change
-		s2.shrink_to_fit(); cap=s2.capacity(); s2.reserve(len); assert(cap==s2.capacity()); //change
-		s2.reserve(len+1); assert(cap!=s2.capacity()); //change
-	}
-
-//-	const char* c_str()
-	//tested above!
+////-	String()
+//	string s0;	assert (s0=="");
+//
+////-	String(Sträng sträng)
+//	string s1("foo"); assert(s1=="foo");
+//	string s2(s1); assert(s2=="foo");
+//	string s3("bar");  assert(s3=="bar");
+//
+////-	~String() Kom ihåg destruktorn!
+//	delete new string("hej");
+//
+////	-	operator =(Sträng sträng)
+//	assert((s2=s3)==s3);
+//	assert((s2=s2)==s3);
+//	assert((s2=("foo"))=="foo");
+//	assert((s2="bar")=="bar");
+//
+////-	operator+=(Sträng sträng) som tolkas som konkatenering.
+//	//foo, bar, bar
+//	(s2+=s1)+=(s3+=s1);
+//	assert(s3=="barfoo" && s2=="barfoobarfoo" && s1=="foo");
+//
+//	//+= som får plats;
+//	s3="bar"; s3.reserve(10);
+//	s3+=s1;
+//	assert(s3=="barfoo");
+//
+//	//+= som inte får plats;
+//	s3="bar"; s3.reserve(5);
+//	s3+=s1;
+//	assert(s3=="barfoo");
+//
+//	//+= som får plats; Själv
+//	s3="bar"; s3.reserve(10);
+//	s3+=s3;
+//	assert(s3=="barbar");
+//
+//	//+= som inte får plats; Själv
+//	s3="bar"; s3.reserve(5);
+//	s3+=s3;
+//	assert(s3=="barbar");
+//
+////-	operator+ räcker med bara String+String
+//	s2="bar";
+//	//auto sss=s1+s2;
+//	//sss=="foobar";
+//	//assert(sss=="foobar");
+//	////assert(s1+s2=="foobar" && s1=="foo");
+//	assert(s1+s2=="foobar" && s1=="foo");
+//
+////-	operator== räcker med String==Sträng
+//	//testas överallt!
+//
+////-	at(int i) som indexerar med range check
+//	try {
+//	 s2.at(-1);
+//	 assert(false);
+//	} catch (std::out_of_range&) {};
+//	try {
+//	 s2.at(3);
+//	 assert(false);
+//	} catch (std::out_of_range&) {};
+//	assert(s2.at(2)='r');
+//
+////-	operator[](int i) som indexerar utan range check.
+//	s2[-1]; s2[1000];
+//	assert(s2[1]=='a');
+//
+////-	push_back(char c) lägger till ett tecken sist.
+//	s2.push_back('a');
+//	assert(s2=="bara");
+//
+//	//-	length(), reserve(), capacity() och shrink_to_fit() är funktioner som finns i container klasserna i STL.
+//
+//	int len=s2.length();
+//	s2.shrink_to_fit();
+//	assert(s2.length()==s2.capacity());
+//
+//	s2.data();
+//	if(s2.length()==s2.capacity()) {
+//		//lagrar strängen med \0
+//		const char * p1 = s2.data();
+//		s2.reserve(len); assert(p1==s2.data()); //no change
+//		p1= s2.data(); s2.reserve(len+1); assert(p1!=s2.data()); //change
+//		p1= s2.data(); s2.shrink_to_fit();  assert(p1!=s2.data()); //change
+//		p1= s2.data(); s2.shrink_to_fit();  assert(p1==s2.data()); //no change
+//	} else {
+//		//lagrar strängen utan \0
+//		int cap;
+//		s2.data(); cap=s2.capacity(); s2.shrink_to_fit(); assert(cap!=s2.capacity()); //change
+//		cap=s2.capacity(); s2.data(); assert(cap!=s2.capacity()); //change
+//		s2.shrink_to_fit(); cap=s2.capacity(); s2.reserve(len); assert(cap==s2.capacity()); //change
+//		s2.reserve(len+1); assert(cap!=s2.capacity()); //change
+//	}
+//
+////-	const char* c_str()
+//	//tested above!
 
 }
 
@@ -159,25 +159,25 @@ void TestFörVälGodkäntString() {
 
 }
 
-
-//#define MACROTestIttPart(CR)							\
-//	/*	*it, ++it, it++, (it+i), it[i], == och !=	*/	\
-//	void TestIttPart##CR() {							\
-//	String s1("foobar");								\
-//	for (auto i=s1.CR##begin(); i!=s1.CR##end(); i++)	\
-//		cout << *i;										\
-//	cout << endl;										\
-//	if (#CR=="r" || #CR=="cr")							\
-//		s1="raboof";									\
-//	auto it = s1.CR##begin();							\
-//	assert(*it=='f');									\
-//	assert(*(it++)=='f' && *it == 'o');					\
-//	++it;												\
-//	assert(*++it=='b');									\
-//	assert(*(it+1)=='a');								\
-//	assert(it[2]=='r');									\
-//}
 //
+//#define MACROTestIttPart(CR)							
+//	/*	*it, ++it, it++, (it+i), it[i], == och !=	*/	
+//	void TestIttPart##CR() {							
+//	String s1("foobar");								
+//	for (auto i=s1.CR##begin(); i!=s1.CR##end(); i++)	
+//		cout << *i;										
+//	cout << endl;										
+//	if (#CR=="r" || #CR=="cr")							
+//		s1="raboof";									
+//	auto it = s1.CR##begin();							
+//	assert(*it=='f');									
+//	assert(*(it++)=='f' && *it == 'o');					
+//	++it;												
+//	assert(*++it=='b');									
+//	assert(*(it+1)=='a');								
+//	assert(it[2]=='r');									
+//}
+
 //MACROTestIttPart(,);	//, ger tomt argument
 //MACROTestIttPart(c);
 //MACROTestIttPart(r);
@@ -186,23 +186,23 @@ void TestFörVälGodkäntString() {
 
 void TestFörGodkäntItt() {
 
-////-	typdefs för iterator, const_iterator,  reverse_iterator och const_revers_iterator
-//	String::iterator Str;
-//	String::const_iterator cStr;
-//	String::reverse_iterator rStr;
-//	String::const_reverse_iterator crStr;
-//
-////-	funktionerna begin, end, cbegin, cend, rbegin, rend, crbegin och crend.
-//
-////Iteratorerna ska kunna göra:
-////-	*it, ++it, it++, (it+i), it[i], == och !=
-//	TestIttPart();
-//	TestIttPartc();
-//	TestIttPartr();
-//	TestIttPartcr();
-//
-//
-////-	default constructor, copy constructor och tilldelning (=) 
+//-	typdefs för iterator, const_iterator,  reverse_iterator och const_revers_iterator
+	//String::iterator Str;
+	//String::const_iterator cStr;
+	//String::reverse_iterator rStr;
+	//String::const_reverse_iterator crStr;
+
+//-	funktionerna begin, end, cbegin, cend, rbegin, rend, crbegin och crend.
+
+//Iteratorerna ska kunna göra:
+//-	*it, ++it, it++, (it+i), it[i], == och !=
+	//TestIttPart();
+	//TestIttPartc();
+	//TestIttPartr();
+	//TestIttPartcr();
+
+
+//-	default constructor, copy constructor och tilldelning (=) 
 //	String s("foobar");
 //	Str=s.begin();
 //	cStr=s.cbegin();
@@ -214,6 +214,11 @@ void TestFörGodkäntItt() {
 ////	*(crStr+3)='d';	//Sak ge kompileringsfel!
 //	assert(s=="aoocar");
 
+	Iterator<string> *it;
+	
+	string s;
+	s = "hej";
+	cout << s;
 }
 
 int main() {
