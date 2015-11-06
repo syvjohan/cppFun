0f9b7017dfe5cfe070ad18fd4fd0fd4597cb8cd7
syvjohan <johanfredriksson1987@gmail.com>
2015-11-06 12:19:26 +0100

    update

--- Inlämningar/SharedPtr/SharedPtr/Defs.h
+++ Inlämningar/SharedPtr/SharedPtr/Defs.h
@@ -1,11 +1,10 @@
-//AB5785 Johan Fredriksson
 #ifndef DEFS_H
 #define DEFS_H
 
 #include <crtdbg.h>
 
 #ifdef _DEBUG
-	#define DBG_NEW new(1, __FILE__, __LINE__)
+	#define DBG_NEW new(1, __FILE__, __LINE__) //Observe: replace new with DBG_NEW.
 #else
 	#define DBG_NEW new
 #endif
